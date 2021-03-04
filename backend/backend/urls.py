c2c2dd6407d3110fc5340bb672fc8b3f10246bd0
LorinLex <laizybot@gmail.com>
2021-03-04 13:48:30 +0300

    Installed swagger urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import url, path
+from django.conf.urls import url
+from django.urls import path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -31,10 +32,6 @@ schema_view = get_schema_view(
    public=True,
    permission_classes=(permissions.AllowAny,),
 )
-
-urlpatterns = [
-  ...
-]
 
 urlpatterns = [
     url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
