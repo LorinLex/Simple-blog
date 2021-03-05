b89377e68633e30c99c750d5c234e03edc002bc3
LorinLex <laizybot@gmail.com>
2021-03-05 17:26:39 +0300

    Added some view changes

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 # Create your views here.
+
+class Posts(APIView):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
