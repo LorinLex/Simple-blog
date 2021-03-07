f960b208416aa120485e9f1e9d3cd571571a8b52
LorinLex <laizybot@mail.com>
2021-03-07 19:58:47 +0300

    Added Tag to admin

--- backend/posts/admin.py
+++ backend/posts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, PostLike, PostDislike
+from .models import Post, PostLike, PostDislike, Tag
 # Register your models here.
 
 admin.site.register(Post)
 admin.site.register(PostLike)
 admin.site.register(PostDislike)
+admin.site.register(Tag)
