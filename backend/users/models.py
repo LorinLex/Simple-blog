7eeda46772c568ffba84f99bb611ffe749c9dcd3
LorinLex <laizybot@gmail.com>
2021-03-17 13:47:31 +0300

    Added profile model

--- backend/users/models.py
+++ backend/users/models.py
@@ -3,6 +3,17 @@ from posts.models import Post, Tag
 from django.contrib.auth.models import User
 # Create your models here.
 
-class UserDetail(models.Model):
-    status = models.CharField(max_length=100)
-    liked_tags = models.ManeToMany(Tag, blank=True)
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    img = models.ImageField(upload_to='avatars/', blank=True)
+    status = models.CharField(max_length=100, blank=True)
+    subscribe_tags = models.ManyToManyField(Tag, blank=True, related_name='subscribe_tags')
+    favourite_posts = models.ManyToManyField(Post, blank=True, related_name='favourite_posts')
+    user_posts = models.ManyToManyField(Post, blank=True, related_name='user_posts')
+    subscribe_users = models.ManyToManyField(User, blank=True, related_name='subscribe_users')
+    likes = models.ManyToManyField(User, blank=True, related_name='likes_from_users')
+    dislikes = models.ManyToManyField(User, blank=True, related_name='dislikes_from_users')
